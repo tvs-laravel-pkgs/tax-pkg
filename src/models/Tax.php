a7697e9078b4aa156473fa23a70c3597d6918332
Abdul <abdul@uitouxsolutions.com>
2019-11-11 11:14:29 +0530

    seeder

--- src/models/Tax.php
+++ src/models/Tax.php
@@ -2,6 +2,8 @@
 
 namespace Abs\TaxPkg;
 
+use App\Company;
+use App\Config;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -13,5 +15,53 @@ class Tax extends Model {
 		'updated_by_id',
 		'deleted_by_id',
 	];
+
+	public static function createFromObject($record_data) {
+
+		$errors = [];
+		$company = Company::where('code', $record_data->company)->first();
+		if (!$company) {
+			dump('Invalid Company : ' . $record_data->company);
+			return;
+		}
+
+		$admin = $company->admin();
+		if (!$admin) {
+			dump('Default Admin user not found');
+			return;
+		}
+
+		$type = Config::where('name', $record_data->type)->where('config_type_id', 88)->first();
+		if (!$type) {
+			$errors[] = 'Invalid Tax Type : ' . $record_data->type;
+		}
+
+		if (count($errors) > 0) {
+			dump($errors);
+			return;
+		}
+
+		$record = self::firstOrNew([
+			'company_id' => $company->id,
+			'name' => $record_data->tax_name,
+		]);
+		$record->type_id = $type->id;
+		$record->created_by_id = $admin->id;
+		$record->save();
+		return $record;
+	}
+
+	public static function createFromCollection($records) {
+		foreach ($records as $key => $record_data) {
+			try {
+				if (!$record_data->company) {
+					continue;
+				}
+				$record = self::createFromObject($record_data);
+			} catch (Exception $e) {
+				dd($e);
+			}
+		}
+	}
 
 }
--- src/models/TaxCode.php
+++ src/models/TaxCode.php
@@ -1,17 +1,81 @@
 <?php
 
 namespace Abs\TaxPkg;
-
+use App\Company;
+use App\Config;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TaxCode extends Model {
 	use SoftDeletes;
-	protected $table = 'taxe_codes';
+	protected $table = 'tax_codes';
 	protected $fillable = [
 		'created_by_id',
 		'updated_by_id',
 		'deleted_by_id',
 	];
+
+	public function taxes() {
+		return $this->belongsToMany('Abs\TaxPkg\Tax', 'tax_code_tax');
+	}
+
+	public static function createFromObject($record_data) {
+
+		$errors = [];
+		$company = Company::where('code', $record_data->company)->first();
+		if (!$company) {
+			dump('Invalid Company : ' . $record_data->company);
+			return;
+		}
+
+		$admin = $company->admin();
+		if (!$admin) {
+			dump('Default Admin user not found');
+			return;
+		}
+
+		$type = Config::where('name', $record_data->type)->where('config_type_id', 82)->first();
+		if (!$type) {
+			$errors[] = 'Invalid type : ' . $record_data->type;
+		}
+
+		$tax = Tax::where('name', $record_data->tax)->where('company_id', $company->id)->first();
+		if (!$tax) {
+			$errors[] = 'Invalid tax : ' . $record_data->tax;
+		}
+
+		if (count($errors) > 0) {
+			dump($errors);
+			return;
+		}
+
+		$record = self::firstOrNew([
+			'company_id' => $company->id,
+			'code' => $record_data->code,
+		]);
+		$record->type_id = $type->id;
+		$record->created_by_id = $admin->id;
+		$record->save();
+
+		$record->taxes()->syncWithoutDetaching([
+			$tax->id => [
+				'percentage' => $record_data->percentage,
+			],
+		]);
+		return $record;
+	}
+
+	public static function createFromCollection($records) {
+		foreach ($records as $key => $record_data) {
+			try {
+				if (!$record_data->company) {
+					continue;
+				}
+				$record = self::createFromObject($record_data);
+			} catch (Exception $e) {
+				dd($e);
+			}
+		}
+	}
 
 }
