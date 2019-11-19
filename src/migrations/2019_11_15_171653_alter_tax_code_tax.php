e79c3e5356e5d8fab33fb756be71f5ad47de7555
Vijay S <vijay@uitoux.in>
2019-11-19 12:43:47 +0530

    migrateion for tax codes pivot

---
+++ src/migrations/2019_11_15_171653_alter_tax_code_tax.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTaxCodeTax extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('tax_code_tax', function (Blueprint $table) {
+			$table->integer('state_id')->nullable()->after('percentage');
+			$table->foreign('state_id')->references('id')->on('states')->onDelete('cascade')->onUpdate('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('tax_code_tax', function (Blueprint $table) {
+			$table->dropForeign('tax_code_tax_state_id_foreign');
+			$table->dropColumn('state_id');
+		});
+	}
+}
