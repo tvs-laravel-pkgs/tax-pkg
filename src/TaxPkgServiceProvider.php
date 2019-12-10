b8a7441886ec4f3828e8c51e97a105d9ab4f0fb5
Abdul <abdul@uitoux.in>
2019-12-10 15:05:13 +0530

    Update TaxPkgServiceProvider.php

--- src/TaxPkgServiceProvider.php
+++ src/TaxPkgServiceProvider.php
@@ -14,9 +14,7 @@ class TaxPkgServiceProvider extends ServiceProvider {
 		$this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 		$this->loadRoutesFrom(__DIR__ . '/routes/api.php');
 		$this->loadMigrationsFrom(__DIR__ . '/migrations');
-		$this->loadViewsFrom(__DIR__ . '/views', 'basic');
 		$this->publishes([
-			__DIR__ . '/views' => base_path('resources/views/tax-pkg'),
 			__DIR__ . '/public' => base_path('public'),
 		]);
 	}
@@ -27,7 +25,5 @@ class TaxPkgServiceProvider extends ServiceProvider {
 	 * @return void
 	 */
 	public function boot() {
-		// $this->app->make('Abs\Basic\EntityController');
-		// $this->app->make('Abs\Basic\API\AuthController');
 	}
 }
