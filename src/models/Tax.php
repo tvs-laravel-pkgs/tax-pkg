69358f465fb2d10ec8a99ce3aee2667f4bc323c0
Vijay-UITOUX <52068840+Vijay-UITOUX@users.noreply.github.com>
2020-06-26 16:32:53 +0530

    KFC Update for service invoice

--- src/models/Tax.php
+++ src/models/Tax.php
@@ -72,13 +72,14 @@ class Tax extends Model {
 				//INTER STATE && STATE SPECIFIC(IF CUSTOMER STATE MATCHES)
 				$general_taxes = self::where('type_id', 1161)->pluck('id')->toArray();
 			}
-			$statespec_tax = self::where('type_id', 1162)->first();
-			if ($statespec_tax && $serviceItem->taxCode) {
-				$state_specifi_tax = $serviceItem->taxCode->taxes()->where('state_id', $customer_state_id)->pluck('tax_id')->toArray();
-			} else {
-				$state_specifi_tax = [];
-			}
-			$taxes = array_unique(array_merge($general_taxes, $state_specifi_tax));
+			// $statespec_tax = self::where('type_id', 1162)->first();
+			// if ($statespec_tax && $serviceItem->taxCode) {
+			// 	$state_specifi_tax = $serviceItem->taxCode->taxes()->where('state_id', $customer_state_id)->pluck('tax_id')->toArray();
+			// } else {
+			// 	$state_specifi_tax = [];
+			// }
+			// $taxes = array_unique(array_merge($general_taxes, $state_specifi_tax));
+			$taxes = $general_taxes;
 
 		} else {
 			$taxes = [];
